3eaef0abee271fe86142dfad83260b5c8f4fb577
Cyan Huynh <cyanhuynh10@gmail.com>
2026-09-15 18:35:04 +0700

    initialize T-Winkle Spring Boot project

---
+++ src/main/java/cyan/project/twinkle/TWinkleApplication.java
@@ -0,0 +1,13 @@
+package cyan.project.twinkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWinkleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TWinkleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cyan/project/twinkle/TWinkleApplicationTests.java
@@ -0,0 +1,13 @@
+package cyan.project.twinkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TWinkleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
